2582641347f3089906f6a527935da24fe39b1fb8
ShubhamMandekar <shubhammandekar777@gmail.com>
2024-10-13 13:07:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitPushPrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Git Push Project it create new repo");
+        }
+    }
+}
